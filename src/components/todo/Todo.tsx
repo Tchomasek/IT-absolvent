ddf167815fd492edf6c3341cd74636fd96496431
Tom <tchom.asek@centrum.cz>
2021-07-23 15:58:41 +0200

    basic functionality complete

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -10,6 +10,8 @@ interface State {
   newTodo: string,
   nextId: number,
   filter: string
+  // enableEdit: boolean,
+  // valueOfInput: string
 }
 
 class TodoList extends React.Component<Props, State>{
@@ -19,18 +21,22 @@ class TodoList extends React.Component<Props, State>{
       todos: [],
       newTodo: '',
       nextId: 0,
-      filter: 'all'
+      filter: 'all',
+      // enableEdit: false,
+      // valueOfInput: ''
     };
-    this.handleToggle = this.handleToggle.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleCheckboxToggle = this.handleCheckboxToggle.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.enableEditToggle = this.enableEditToggle.bind(this);
   }
 
   onSubmit(e){
     e.preventDefault()
-    const newTodoArray = {id:this.state.nextId,text:this.state.newTodo,completed:false}
+    const newTodoArray = {id:this.state.nextId,text:this.state.newTodo,completed:false, enableEdit:false}
     this.setState(prevState =>{
       return {
         todos: this.state.todos.concat(newTodoArray),
@@ -40,7 +46,7 @@ class TodoList extends React.Component<Props, State>{
     })
   }
 
-  handleToggle(id) {
+  handleCheckboxToggle(id) {
     this.setState((prevState) => {
       const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
@@ -57,7 +63,7 @@ class TodoList extends React.Component<Props, State>{
     });
   }
 
-  handleChange(e){
+  handleInputChange(e){
     this.setState({newTodo: e.target.value})
   }
 
@@ -74,28 +80,94 @@ class TodoList extends React.Component<Props, State>{
     })
   }
 
+  handleTextChange(e, id){
+    const newTodos = this.state.todos.map(item => {
+      if (item.id === id){
+        return {
+          ...item,
+          text: e.target.value
+        }
+      }
+      return item
+    })
+    this.setState({
+      todos: newTodos
+    })
+  }
+
+  enableEditToggle(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+            return {
+                ...todo,
+                enableEdit: !todo.enableEdit
+            }
+        }
+        return todo
+    })
+    console.log(updatedTodos)
+
+      return {
+        todos: updatedTodos,
+      };
+    });
+  }
+  
+
   render() {
     const todoItems = this.state.todos.map((item) => {
       if (this.state.filter === 'all'){
-        return <TodoItem key={item.id} id={item.id} text={item.text} completed={item.completed} handleChange={this.handleToggle} handleDelete={this.handleDelete}/>
+        return <TodoItem 
+          key={item.id} 
+          id={item.id} 
+          text={item.text} 
+          completed={item.completed} 
+          handleCheckboxToggle={this.handleCheckboxToggle} 
+          handleDelete={this.handleDelete} 
+          handleTextChange={this.handleTextChange}
+          enableEdit={item.enableEdit}
+          enableEditToggle={this.enableEditToggle}
+        />
       }
       if (this.state.filter === 'active'){
         if (item.completed === false){
-          return <TodoItem key={item.id} id={item.id} text={item.text} completed={item.completed} handleChange={this.handleToggle} handleDelete={this.handleDelete}/>
+          return <TodoItem 
+            key={item.id} 
+            id={item.id} 
+            text={item.text} 
+            completed={item.completed}
+            handleCheckboxToggle={this.handleCheckboxToggle} 
+            handleDelete={this.handleDelete} 
+            handleTextChange={this.handleTextChange}
+            enableEdit={item.enableEdit}
+            enableEditToggle={this.enableEditToggle}
+          />
         }
       }
       if (this.state.filter === 'completed'){
         if (item.completed === true){
-          return <TodoItem key={item.id} id={item.id} text={item.text} completed={item.completed} handleChange={this.handleToggle} handleDelete={this.handleDelete}/>
+          return <TodoItem 
+            key={item.id} 
+            id={item.id} 
+            text={item.text} 
+            completed={item.completed} 
+            handleCheckboxToggle={this.handleCheckboxToggle} 
+            handleDelete={this.handleDelete} 
+            handleTextChange={this.handleTextChange}
+            enableEdit={item.enableEdit}
+            enableEditToggle={this.enableEditToggle}
+          />
         }      
       }
     });
 
-
+    
     return (
       <div className="todo-list">
+        
         <form onSubmit={this.onSubmit}>
-          <input type='text' autoFocus value={this.state.newTodo} onChange={this.handleChange}/>
+          <input type='text' autoFocus value={this.state.newTodo} onChange={this.handleInputChange}/>
         </form>
         {todoItems}
         <button value='all' onClick={this.handleClick}>All</button>
--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -5,20 +5,71 @@ type todoProps = {
   id: number;
   text: string;
   completed: boolean;
-  handleChange: any;
-  handleDelete: any
+  handleCheckboxToggle: any;
+  handleDelete: any;
+  handleTextChange: any;
+  enableEdit: boolean;
+  enableEditToggle: any
 };
 
-const TodoItem = ({ id, text, completed, handleChange,handleDelete }: todoProps) => (
+
+const TodoItem = ({ id, text, completed, enableEdit, handleCheckboxToggle, handleDelete, handleTextChange, enableEditToggle }: todoProps) => {
+  return (
   <div className="todo-item">
     <input
       type="checkbox"
       checked={completed}
-      onChange={() => handleChange(id)}
+      onChange={() => handleCheckboxToggle(id)}
     />
-    {text}
+    {enableEdit ? (
+      <input 
+        type='text'
+        value={text}
+        onChange={(e) => handleTextChange(e, id)}
+        onKeyDown={(event) => {
+              if (event.key === 'Enter' || event.key === 'Escape') {
+                console.log(id)
+                enableEditToggle(id)
+                event.preventDefault()
+                event.stopPropagation()
+              }
+            }}
+      />
+    ) : (
+      <p
+    onDoubleClick={() => enableEditToggle(id)}
+  >{text}</p>
+    )}
     <button value={id} onClick={handleDelete}>x</button>
   </div>
 );
+}
 
 export default TodoItem;
+
+// {this.state.enableEdit ? (
+//   <input
+//   type="text"
+//   value={this.state.valueOfInput}
+//   onChange={(event) => {
+//     this.setState({
+//       valueOfInput: event.target.value
+//     })
+//   }}
+//   onKeyDown={(event) => {
+//     if (event.key === 'Enter' || event.key === 'Escape') {
+//       this.setState({
+//         enableEdit: false
+//       })
+//       event.preventDefault()
+//       event.stopPropagation()
+//     }
+//   }}
+// />         
+// ) : (
+//   <p
+//     onDoubleClick={() => this.setState({
+//       enableEdit: true
+//     })}
+//   >{this.state.valueOfInput}x</p>
+// )}
