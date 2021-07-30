f7186fa3a43e92fd45127d118b0f9f6a5dff1bad
Tom <tchom.asek@centrum.cz>
2021-07-30 12:32:08 +0200

    functional tictactoe without required technology

--- src/components/tic-tac-toe/Square.tsx
+++ src/components/tic-tac-toe/Square.tsx
@@ -2,28 +2,21 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 type squareProps = {
-  handleClick: any;
+  handleClick: () => void;
   value: string;
 };
 
-export default class Square extends Component<squareProps, {}> {
-  render() {
-    const Td = styled.td`
-    width: "50px",
-    height: "50px",
-    border: "1px solid black",
-    `;
-    return (
-      <Td
-        style={{
-          width: "50px",
-          height: "50px",
-          border: "1px solid black",
-        }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.value}
-      </Td>
-    );
+const MyTd = styled.td`
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+  &:hover {
+    background-color: #d4c7c7;
   }
-}
+`;
+
+const Square = React.memo((props: squareProps) => {
+  return <MyTd onClick={props.handleClick}>{props.value}</MyTd>;
+});
+
+export default Square;
--- src/components/tic-tac-toe/TicTacToe.tsx
+++ src/components/tic-tac-toe/TicTacToe.tsx
@@ -7,6 +7,16 @@ type State = {
   nextTurn: string;
 };
 
+const Table = styled.table`
+    text-align: "center",
+    margin: "auto",
+    height: "auto",
+    width: "500px",
+    /* border: "1px solid black", */
+    /* tableLayout: "fixed", */
+    display: "flex"
+  `;
+
 export default class TicTacToe extends Component<{}, State> {
   constructor(props) {
     super(props);
@@ -17,45 +27,100 @@ export default class TicTacToe extends Component<{}, State> {
       nextTurn: "O",
     };
     this.handleClick = this.handleClick.bind(this);
-    this.changeNextTurn = this.changeNextTurn.bind(this);
+    this.reset = this.reset.bind(this);
+    /* this.changeNextTurn = this.changeNextTurn.bind(this); */
   }
 
-  changeNextTurn() {
+  /* changeNextTurn() {
     this.setState((prevState) => {
       const nextTurn = prevState.nextTurn === "O" ? "X" : "O";
       return { nextTurn: nextTurn };
     });
-  }
-
-  handleClick(i, j) {
-    this.changeNextTurn();
-    this.setState((prevState) => {
-      const newGrid = prevState.grid.map((row, i_index) => {
-        const newRow = row.map((square, j_index) => {
-          if (i === i_index && j === j_index && square === "") {
-            return this.state.nextTurn;
-          } else {
-            return square;
-          }
-        });
-        return newRow;
-      });
-      return {
-        ...prevState,
-        grid: newGrid,
-      };
+  } */
+  reset() {
+    this.setState({
+      grid: Array(10)
+        .fill(0)
+        .map(() => Array(10).fill("")),
+      nextTurn: "O",
     });
+  }
+  handleClick(i: number, j: number) {
+    if (
+      this.state.nextTurn === this.state.grid[i][j] ||
+      this.state.grid[i][j] !== ""
+    ) {
+      return;
+    } else {
+      this.setState((prevState) => {
+        const newGrid = prevState.grid.map((row, i_index) => {
+          const newRow = row.map((square, j_index) => {
+            if (i === i_index && j === j_index && square === "") {
+              return this.state.nextTurn;
+            } else {
+              return square;
+            }
+          });
+          return newRow;
+        });
+        return {
+          nextTurn: prevState.nextTurn === "O" ? "X" : "O",
+          grid: newGrid,
+        };
+      });
+    }
+    const g = this.state.grid;
+    // flip clicked square manualy, because this.state.grid returned old, not updated grid
+    g[i][j] = this.state.nextTurn;
+
+    function checkDir(x_, y_, color) {
+      //track how many squares of a given color there are in a given dirention (specified by x_ and y_)
+      //for example checkDir(0,1, 'w') checks how many white stones there are in a row to the right )
+      let tracked = 0;
+      let _x = i;
+      let _y = j;
+      //stop tracking stones when the color is not equal to the specified stone or we have gone past the edge of the board
+      while (g[_x] !== undefined && g[_x][_y] === color) {
+        //increment the number of tracked stones
+        tracked += 1;
+        //increment/decrement to check the next square in the specified direction
+        _y += y_;
+        _x += x_;
+      }
+      return tracked;
+    }
+    //sum the directions (left+right, up+down, 2 diagonals)
+    const w_horizontal = checkDir(0, 1, "X") + checkDir(0, -1, "X") - 1;
+    const b_horizontal = checkDir(0, 1, "O") + checkDir(0, -1, "O") - 1;
+
+    const w_vertical = checkDir(1, 0, "X") + checkDir(-1, 0, "X") - 1;
+    const b_vertical = checkDir(1, 0, "O") + checkDir(-1, 0, "O") - 1;
+
+    const w_diag1 = checkDir(1, 1, "X") + checkDir(-1, -1, "X") - 1;
+    const b_diag1 = checkDir(1, 1, "O") + checkDir(-1, -1, "O") - 1;
+
+    const w_diag2 = checkDir(1, 1, "X") + checkDir(-1, -1, "X") - 1;
+    const b_diag2 = checkDir(-1, 1, "O") + checkDir(1, -1, "O") - 1;
+
+    //check to see if there are any sums greater than or equal to 5 and alert the players of a win
+    //setTimeout is called so that the alert() function does not hold up the rendering of the board.
+    if (w_horizontal >= 5 || w_vertical >= 5 || w_diag1 >= 5 || w_diag2 >= 5) {
+      setTimeout(() => {
+        alert("X wins");
+      }, 1);
+      this.reset();
+    }
+
+    if (b_horizontal >= 5 || b_vertical >= 5 || b_diag1 >= 5 || b_diag2 >= 5) {
+      setTimeout(() => {
+        alert("O wins");
+      }, 1);
+      this.reset();
+    }
   }
 
   render() {
-    const Table = styled.table`
-    textAlign: "center",
-    margin: "auto",
-    height: "auto",
-    width: "500px",
-    border: "1px solid black",
-    tableLayout: "fixed",
-  `;
+    console.time("a");
     const board = this.state.grid.map((row, i) => {
       return (
         <tr key={"row_" + i}>
@@ -71,16 +136,17 @@ export default class TicTacToe extends Component<{}, State> {
         </tr>
       );
     });
-
     return (
-      <div style={{ textAlign: "center" }}>
-        <div style={{ margin: "auto", width: "40%" }}>
+      <div style={{ display: "flex" }}>
+        <div style={{ margin: "auto", width: "40%", textAlign: "center" }}>
           <Table cellSpacing="0">
             <tbody>{board}</tbody>
           </Table>
+          <button onClick={this.reset}>Reset</button>
         </div>
         <br />
       </div>
     );
   }
 }
+console.timeEnd("a");
